dab789da1318e60bf2b15b0a684df863e0d870e7
Ryan C. Collins <rccproductions@mac.com>
2017-09-03 19:53:42 -0400

    Fix: linting issues in withUniqueKey

--- src/withUniqueKey.ts
+++ src/withUniqueKey.ts
@@ -1,6 +1,6 @@
 /* withUniqueKey: (key: string) => HOC
-Add a unique key to each object in an array of 
-objects in order to satisfy [React's unique key requirements](https://facebook.github.io/react/docs/lists-and-keys.html) 
+Add a unique key to each object in an array of
+objects in order to satisfy [React's unique key requirements](https://facebook.github.io/react/docs/lists-and-keys.html)
 when mapping over that array.
 */
 import { mapProps, InferableComponentEnhancerWithProps } from 'recompose'
