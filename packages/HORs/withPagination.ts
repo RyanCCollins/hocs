5c4435aaacc14f55fa8352caaa4b431071308c73
Ryan C. Collins <rccproductions@mac.com>
2017-09-03 21:20:14 -0400

    Feat: add types to withPagination

--- packages/HORs/withPagination.ts
+++ packages/HORs/withPagination.ts
@@ -1,43 +1,30 @@
-import { Reducer } from 'redux'
+import { Reducer, Action } from 'redux'
 
-const withPagination = (actionPrefix) => (reducer) => (state, action) => {
-  if (action.type !== `${actionPrefix}/PAGINATE`) {
-    return reducer(state, action)
-  }
-  switch (action.type) {
-  case `${actionPrefix}/PAGINATE`:
-    return {
-      ...state,
-      pagination: {
-        ...state.pagination,
-        currentPage: state.pagination.currentPage + 1,
-      },
-    }
-  default: return state
-  }
+export interface Pagination {
+  perPage: number
+  currentPage: number
 }
+
+export interface State {
+  pagination: Pagination
+}
+
+const withPagination = (actionPrefix: string) =>
+  (reducer: Reducer<State>) => (state: State, action: Action): State => {
+    if (action.type !== `${actionPrefix}/PAGINATE`) {
+      return reducer(state, action)
+    }
+    switch (action.type) {
+    case `${actionPrefix}/PAGINATE`:
+      return {
+        ...state,
+        pagination: {
+          ...state.pagination,
+          currentPage: state.pagination.currentPage + 1,
+        },
+      }
+    default: return state
+    }
+  }
 
 export default withPagination
-
-/*
-Example:
-const initialState = {
-  pagination: {
-    currentPage: 1,
-    perPage: 8,
-  }
-}
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-  case 'FOO':
-    return {
-      ...state,
-      foo: 'bar',
-    }
-  default: return state
-  }
-}
-
-const enhancedReducer = withPagination()(reducer)
-*/
