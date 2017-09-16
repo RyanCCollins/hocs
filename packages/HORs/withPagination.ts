7f84bcb1cc30d9edd94886402d2669fb3470c59e
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-09-16 15:47:52 -0400

    Update withPagination.ts
    Add selectCurrentItems selector

--- packages/HORs/withPagination.ts
+++ packages/HORs/withPagination.ts
@@ -41,3 +41,13 @@ export const selectPagination = (state: State): Pagination => state.pagination
 
 export const selectCurrentPage = (state: State): number => selectPagination(state).currentPage
 export const selectPerPage = (state: State): number => selectPagination(state).perPage
+
+export const selectCurrentItems = (path: string) => (state: State) => {
+  const items = get(state, path, []);
+  const currentPage = selectCurrentPage(state);
+  const perPage = selectPerPage(state);
+  const current = currentPage - 1;
+  const from = current * perPage;
+  const to = (current * perPage) + perPage;
+  return items.filter((_, i) => i >= from && i < to);
+}
